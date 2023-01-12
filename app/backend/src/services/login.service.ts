411a7773a6e12b1489a4ea49d4344df8adb378ad
Graciele Sampaio <graciele-1996@hotmail.com>
2023-01-12 12:31:18 -0300

    requisito 11

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,12 +1,23 @@
+import * as Bcrypt from 'bcryptjs';
 import JwtEncoded from '../utils/JwtEncoded';
 import UserModel from '../database/models/User.model';
 
 class LoginService {
   jwt = new JwtEncoded();
   loginServiceTwo = async (email: string, password: string) => {
-    const user = await UserModel.findOne({ where: { email } });
-    if (user) return { type: 200, response: { token: this.jwt.createToken({ email, password }) } };
-    return { type: 401, response: { message: 'Incorrect email or password' } };
+    const user: UserModel | null = await UserModel.findOne({ where: { email } });
+
+    if (!user) return { type: 401, response: { message: 'Incorrect email or password' } };
+
+    const encryptedPassword = Bcrypt.compareSync(password, user?.password);
+    console.log(encryptedPassword);
+    console.log(user.password);
+
+    if (!encryptedPassword) {
+      return { type: 401, response: { message: 'Incorrect email or password' } };
+    }
+
+    return { type: 200, response: { token: this.jwt.createToken({ email, password }) } };
   };
 }
 
